268ba1eda93115cc3d57ee31cb5c704545fe76d4
JonFerSanLuis <jonfernandez.sanluis@gmail.com>
2025-05-14 10:54:01 +0000

    Deploying to gh-pages from @ JonFerSanLuis/goikode-multilang-vue@6f4ad05a457849bac7683da4c558886d52c5ff48 🚀

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import ServiciosView from '../views/ServiciosView.vue'
+import ContactoView from '../views/ContactoView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/servicios',
+      name: 'servicios',
+      component: ServiciosView
+    },
+    {
+      path: '/contacto',
+      name: 'contacto',
+      component: ContactoView
+    }
+  ]
+})
+
+export default router
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  base: '/goikode-multilang-vue/', 
+  //publicPath: '/goikode-multilang-vue/',
+  plugins: [vue()]
+})
