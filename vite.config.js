42ce2a483733487139c7810c2196b76f8c5799f5
JonFerSanLuis <jonfernandez.sanluis@gmail.com>
2025-05-14 14:18:59 +0200

    netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/goikode-multilang-vue/', 
+  base: './', 
   plugins: [vue()]
 })
